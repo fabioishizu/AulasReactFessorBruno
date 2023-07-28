4580b1ee43278b23269864d7449592fe4799f9af
Fabio Ishizu <f.masaoishizu@gmail.com>
2023-07-28 15:26:30 -0300

    Aulas 17 e 18

--- Aula001/my-app/src/App.js
+++ Aula001/my-app/src/App.js
@@ -1,22 +1,15 @@
 import React,{useState} from 'react'
-import Nota from './Components/Nota'
-import Resultado from './Components/Resultado'
+import Caixa from './Components/Caixa'
+import Canal from './Components/Canal'
 
 export default function App() {
 
-  const [nota1,setNota1] = useState(0)
-  const [nota2,setNota2] = useState(0)
-  const [nota3,setNota3] = useState(0)
-  const [nota4,setNota4] = useState(0)
-
   return ( 
     <>
-      <Nota num={1} nota={nota1} setNota={setNota1}/>
-      <Nota num={2} nota={nota2} setNota={setNota2}/>
-      <Nota num={3} nota={nota3} setNota={setNota3}/>
-      <Nota num={4} nota={nota4} setNota={setNota4}/>
-
-      <Resultado somaNotas={parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)}/>
+      <Caixa site="teste.com">
+        <Canal/>
+        <p>curso de react</p>
+      </Caixa>
     </>
   );
 }
---
+++ Aula001/my-app/src/Components/Canal.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Canal(props){
+    return(
+        <h1>CFB cursos</h1>
+    )
+}
