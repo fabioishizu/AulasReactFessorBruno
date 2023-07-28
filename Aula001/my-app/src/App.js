ad49ad313c09f3d9b6b7d06ae4e5e499fc64cbfe
Fabio Ishizu <f.masaoishizu@gmail.com>
2023-07-28 14:29:28 -0300

    aula 14

--- Aula001/my-app/src/App.js
+++ Aula001/my-app/src/App.js
@@ -4,11 +4,15 @@ export default function App() {
 
   const carros = ['HRV', 'Golf', 'Focus', 'Cruze', 'Argo', 'Gol']
 
+  const [nome, setNome] = useState('Digite seu nome aqui')
+
   const listaCarros = carros.map( (c,i) => <li key={i}>{i} - {c}</li> )
 
   return ( 
     <>
-      {listaCarros}
+      <label>Digite um nome: </label>
+      <input type="text" name="fnome" value={nome} onChange={(e) => setNome(e.target.value)}/>
+      <p>{nome}</p>
     </>
   );
 }
