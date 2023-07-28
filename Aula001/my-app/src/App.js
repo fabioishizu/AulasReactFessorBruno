e58b0c9d84bc93b6f421291c1837e70edc85e2f1
Fabio Ishizu <f.masaoishizu@gmail.com>
2023-07-28 14:20:15 -0300

    aula 14

--- Aula001/my-app/src/App.js
+++ Aula001/my-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App(){
+
+  const [nome,setNome] = useState('Digite seu nome')
+  const [carro,setCarro] = useState('')
+  const handleChangeNome = (e) => {
+    setNome(e.target.value) 
+  }
+
+  return(
+    <>
+      <label>digite seu nome</label>
+      <input type="text" name="fnome" value={nome} onChange={(e) => setNome(e.target.value)}/>
+      <p>Nome digitado: {nome}</p>
+      <label>Selecione um carro: </label>
+      <select values={carro} onChange={(e) => setCarro(e.target.value)}>
+        <option value="HRV">HRV</option>
+        <option value="Gol">Gol</option>
+        <option value="Cruze">Cruze</option>
+        <option value="Argo">Argo</option>
+      </select>
+      <p>Carro selecionado: {carro}</p>
+    </>
   );
 }
-
-export default App;
