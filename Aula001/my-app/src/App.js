0a7afcd9550dc3584dc5162f47c996d67b939cd0
Fabio Ishizu <fabio.masao@tecdet.com.br>
2023-08-07 10:08:44 -0300

    Aula21

--- Aula001/my-app/src/App.js
+++ Aula001/my-app/src/App.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Classe from './Components/Classe'
 
 export default function App() {
 
-  const [nome,setNome] = useState()
-  
-  const armazenar = (chave,valor) => {
-    localStorage.setItem(chave, valor)
-  }
-
-  const consultar = (chave,valor) => {
-    alert(localStorage.getItem(chave, valor))
-  }
-
-  const apagar = (chave) => {
-    localStorage.removeItem(chave)
-  }
-
   return ( 
     <>
-      <label>Digite um nome: </label><br/>
-      <input type="text" value={nome} onChange={(e)=>setNome(e.target.value)}/><br/>
-      <button onClick={()=>armazenar('ls_nome', nome)}>Gravar nome</button>
-      <button onClick={()=>consultar('ls_nome')}>Consultar nome</button>
-      <button onClick={()=>apagar('ls_nome')}>Apagar nome</button>
+      <h1>Componentes de Classe</h1>
+      <Classe canal="CFB Cursos"/>
     </>
   );
 }
---
+++ Aula001/my-app/src/Components/Classe.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Classe extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <>
+                <h1>Primeiro Component de Classe</h1>
+                <p>Canal: {this.props.canal}</p>
+            </>
+        );
+    }
+}
